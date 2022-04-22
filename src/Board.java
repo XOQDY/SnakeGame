db063f0130f6ee68ada989c9dbe6764e8e4d3db6
Bhokin <bhokin.w@ku.th>
2022-04-22 21:16:26 +0700

    :building_construction:create a constructor and initCell:building_construction:

--- src/Board.java
+++ src/Board.java
@@ -3,4 +3,20 @@ public class Board {
     private Cell [][] cells;
     private int size;
 
+    public Board(int size) {
+        this.size = size;
+        initCell();
+    }
+
+    private void initCell() {
+        cells = new Cell[size][size];
+        for (int row = 0; row < size; ++row) {
+            for (int col = 0; col < size; ++col) {
+                cells[row][col] = new Cell();
+                if (row == 0 || row == size-1 || col == 0 || col == size-1) {
+                    cells[row][col].setWall(true);
+                }
+            }
+        }
+    }
 }
