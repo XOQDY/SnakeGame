17e1c3c4bfb95240627c162ffcb420330c567ac8
XOQDY <numfahmaek@gmail.com>
2022-04-22 21:05:33 +0700

    :memo: add get and set method to attribute in Cell :memo:

--- src/Cell.java
+++ src/Cell.java
@@ -4,4 +4,33 @@ public class Cell {
     private boolean snake;
     private boolean food;
 
+    public Cell() {
+        head = false;
+        snake = false;
+        food = false;
+    }
+
+    public boolean isHead() {
+        return head;
+    }
+
+    public boolean isSnake() {
+        return snake;
+    }
+
+    public boolean isFood() {
+        return food;
+    }
+
+    public void setHead(boolean head) {
+        this.head = head;
+    }
+
+    public void setSnake(boolean snake) {
+        this.snake = snake;
+    }
+
+    public void setFood(boolean food) {
+        this.food = food;
+    }
 }
