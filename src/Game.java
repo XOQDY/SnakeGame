65a69988c7d67bc75654734c57a1e4cd460a713a
Bhokin <bhokin.w@ku.th>
2022-04-22 21:31:19 +0700

    :building_construction:add Game constructor and class UI:building_construction:

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Game extends JFrame {
 
     private Board board;
     private int boardSize;
     private int foodCount;
+    private GridUI gridUI;
+
+    public Game() {
+        board = new Board(boardSize);
+        gridUI = new GridUI();
+        add(gridUI);
+        pack();
+    }
+
+    class GridUI extends JPanel {
+        public static final int CELL_PIXEL_SIZE = 30;
+
+        private Image imageHead;
+        private Image imageSnake;
+        private Image imageWall;
+    }
 
 }
